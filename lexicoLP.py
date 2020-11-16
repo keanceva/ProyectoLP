d08200f19828bee3c3967c2bcefb173ad3ba5497
Joffre Ramirez <joffreramirez.r@hotmail.com>
2020-11-15 21:45:45 -0500

    Añadiendo todas las funciones
    Se añadio todas las funciones

--- lexicoLP.py
+++ lexicoLP.py
@@ -18,7 +18,37 @@ reserved = {
     'echo': 'ECHO',
     'print': 'PRINT',
     'print_r': 'PRINT_R',
-    'var_dump': 'VAR_DUMP'
+    'var_dump': 'VAR_DUMP',
+    'fgets': 'FGETS',
+    'fread': 'FREAD',
+    'fscanf': 'FSCANF',
+    'fpassthru': 'FPASSTHRU',
+    'fgetcsv': 'FGETCSV',
+    'fgetc': 'FGETC',
+    'file_get_contents': 'FILE_GET_CONTENTS',
+    'readfile': 'READFILE',
+    'file': 'FILE',
+    'parse_ini_file': 'PARSE_INI_FILE',
+    'implode': 'IMPLODE',
+    'explode': 'EXPLODE',
+
+    'count': 'COUNT',
+    'sizeof': 'SIZEOF',
+    'array_push': 'ARRAYPUSH',
+    'sort': 'SORT',
+    'asort': 'ASORT',
+    'ksort': 'KSORT',
+    'unset': 'UNSET',
+    'var_export': 'VAR_EXPORT',
+    'shuffle': 'SUFFLE',
+    'array_merge': 'ARRAY_MERGE',
+    'array_search': 'ARRAY_SEARCH',
+    'array_rand': 'ARRAY_RAND',
+    'array_chunk': 'ARRAY_CHUNK',
+    'str_split': 'STR_SPLIT',
+    'preg_split': 'PREG_SPLIT',
+    'array_unique': 'ARRAY_UNIQUE',
+
 }
 
 
@@ -137,6 +167,99 @@ def t_PRINT_R(t):
 def t_VAR_DUMP(t):
     r'var_dump'
     return t
+
+
+def t_FGETS(t):
+    r'fgets'
+    return t
+def t_FREAD(t):
+    r'fread'
+    return t
+def t_FSCANF(t):
+    r'fscanf'
+    return t
+def t_FPASSTHRU(t):
+    r'fpassthru'
+    return t
+def t_FGETCSV(t):
+    r'fgetcsv'
+    return t
+def t_FGETC(t):
+    r'fgetc'
+    return t
+def t_FILE_GET_CONTENTS(t):
+    r'file_get_contents'
+    return t
+def t_READFILE(t):
+    r'readfile'
+    return t
+def t_FILE(t):
+    r'file'
+    return t
+def t_PARSE_INI_FILE(t):
+    r'parse_ini_file'
+    return t
+
+def t_IMPLODE(t):
+    r'implode'
+    return t
+def t_EXPLODE(t):
+    r'explode'
+    return t
+def t_COUNT(t):
+    r'count'
+    return t
+def t_SIZEOF(t):
+    r'sizeof'
+    return t
+def t_ARRAY_PUSH(t):
+    r'array_push'
+    return t
+def t_SORT(t):
+    r'sort'
+    return t
+def t_ASORT(t):
+    r'asort'
+    return t
+def t_KSORT(t):
+    r'ksort'
+    return t
+def t_UNSER(t):
+    r'unset'
+    return t
+def t_VAR_EXPORT(t):
+    r'var_export'
+    return t
+def t_FUNCTION(t):
+    r'function'
+    return t
+def t_SHUFFLE(t):
+    r'shuffle'
+    return t
+def t_ARRAY_MERGE(t):
+    r'array_merge'
+    return t
+def t_ARRAY_SEARCH(t):
+    r'array_search'
+    return t
+def t_ARRAY_RAND(t):
+    r'array_rand'
+    return t
+def t_ARRAY_CHUNK(t):
+    r'array_chunk'
+    return t
+def t_STR_SPLIT(t):
+    r'str_split'
+    return t
+def t_PREG_SPLIT(t):
+    r'preg_split'
+    return t
+def t_ARRAY_UNIQUE(t):
+    r'array_unique'
+    return t
+
+
+
 
 def t_error(t):
     print("No es reconocido '%s'"%t.value[0])
